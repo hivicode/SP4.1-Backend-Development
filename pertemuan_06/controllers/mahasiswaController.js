228c34ee064b1a85eaa93dd095ca09ff2a495c43
hivicode <bintangazzuri08@gmail.com>
2026-04-17 21:49:52 +0700

    fix: add pertemuan 05 and 06

---
+++ pertemuan_06/controllers/mahasiswaController.js
@@ -0,0 +1,20 @@
+const Mahasiswa = require("../models/Mahasiswa");
+
+module.exports = {
+  viewMahasiswa: async (req, res) => {
+    try {
+      const mahasiswa = await Mahasiswa.find();
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+
+      res.render("index", {
+        mahasiswa,
+        alert,
+        title: "CRUD",
+      });
+    } catch (error) {
+      res.redirect("/mahasiswa");
+    }
+  },
+};
